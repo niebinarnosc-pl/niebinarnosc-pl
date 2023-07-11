40c47a5f7de24aa15857bd9746c51654fb4f3e70
cddlee <pleasejustwanttocuddle+github@gmail.com>
2023-07-11 14:37:41 +0200

    correction to form

--- src/pages/kontakt/index.js
+++ src/pages/kontakt/index.js
@@ -5,14 +5,24 @@ import Seo from "../../components/Seo";
 
 export default function Kontakt() {
     const [userEmail, setUserEmail] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch("/", {
+          method: "POST",
+          headers: {"Content-Type": "application/x-www-form-urlencoded"},
+          body: new URLSearchParams(new FormData(e.target)).toString()
+        })
+        .then(() => console.log("thank you"))
+        .catch((error) => console.error(error));
+    }
     return <Page className={"kontakt"} heading={"Kontakt"}>
         <p className="font-weight-500">Masz do nas pytania? Szukasz pomocy? Napisz do nas na maila lub wypełnij formularz.</p>
         <p><a href="mailto:kontakt@niebinarnosc.pl" className="contact-mail"><IconEmail/>kontakt@niebinarnosc.pl</a></p>
-        <form name="Kontakt" method="POST" data-netlify="true" netlify-honeypot="bot-field" data-netlify-recaptcha="true">
-            <input type="hidden" name="subject" data-remove-prefix value={`Wiadomość od ${userEmail}`} />
-            <label><IconEmail/><input type="email" placeholder="E-mail, np. nazwa@gmail.com" onChange={e => setUserEmail(e.target.value)}/></label>
-            <textarea placeholder="Tutaj wpisz swoją wiadomość"/>
-            <div className="align-self-flex-end" data-netlify-recaptcha="true"/>
+        <form name="Kontakt" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={handleSubmit}>
+            <input type="hidden" name="form-name" value="Kontakt"/>
+            <input type="hidden" name="subject" data-remove-prefix value={`Wiadomość od ${userEmail}`}/>
+            <label><IconEmail/><input type="email" placeholder="E-mail, np. nazwa@gmail.com" onChange={e => setUserEmail(e.target.value)} required/></label>
+            <textarea placeholder="Tutaj wpisz swoją wiadomość" required/>
             <label className="display-none">Nie wypełniaj, jeśli jesteś niebinarna_y:<input name="bot-field"/></label>
             <button type="submit" className="button">Wyślij wiadomość</button>
         </form>
