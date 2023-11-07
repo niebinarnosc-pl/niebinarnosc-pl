30909bb6a60275ebc63fc8edfc79eb0410c68e8f
cddlee <pleasejustwanttocuddle+github@gmail.com>
2023-11-07 17:56:24 +0100

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,11 @@ import Page from "../components/Page";
 
 export default function e404() {
     return (
-        <Page className={"e404"} heading={"404"} heading2={"Nie znaleziono"}>
-            <blockquote>"Pójdzie do pokoju numer 301, dostanie tam numerek i ustawi się w kolejce do pokoju numer 200, tak? Tam powiedzą co dalej, do widzenia.  ... DRZWI! to jest PRXYCHODNIA nie SUPERMARKET!1"</blockquote>
-            <cite>Miła pani z pokoju 404 :)</cite>
+        <Page className={"e404"} heading={"404"} subheading={"Nie znaleziono"}>
+            <div className="container">
+                <blockquote>"Tu? Tu <em>nic</em> nie ma. Pójdzie do pokoju numer 301, dostanie tam numerek i ustawi się w kolejce do pokoju numer 200, tak? Tam powiedzą co dalej, do widzenia.  ... DRZWI! to jest PRXYCHODNIA nie SUPERMARKET!1"</blockquote>
+                <cite>Miła pani z pokoju 404 :)</cite>
+            </div>
         </Page>
     )
 }
