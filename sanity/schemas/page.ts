1f9b7af746dd4aa06cedb1b6cd5327b26130f0dd
cddlee <pleasejustwanttocuddle+github@gmail.com>
2025-09-11 19:42:04 +0200

    add subheading to page schema

--- sanity/schemas/page.ts
+++ sanity/schemas/page.ts
@@ -11,6 +11,11 @@ export default defineType({
       type: 'string',
     }),
     defineField({
+      name: 'subheading',
+      title: 'Subheading',
+      type: 'string',
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
